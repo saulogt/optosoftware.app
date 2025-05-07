c46b99396a544d0018cccd87a09f75b97f455c58
Saulo Tauil <saulogt@gmail.com>
2025-05-07 10:55:03 -0700

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://www.optosoftware.app",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
